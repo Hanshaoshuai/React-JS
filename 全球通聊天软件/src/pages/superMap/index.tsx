5c1555cc89cc887a6a1572fab252ab6c36c9e872
hanshaoshuai <361062939@qq.com>
2022-02-21 19:03:47 +0800

    feat:add

--- 全球通聊天软件/src/pages/superMap/index.tsx
+++ 全球通聊天软件/src/pages/superMap/index.tsx
@@ -14,6 +14,7 @@ declare global {
   }
 }
 let cameraChangedListener: any = null;
+let viewer: any = null;
 const SuperMap = () => {
   const history = useHistory();
   const infoBoxContainer = useRef<HTMLDivElement>(null);
@@ -22,7 +23,6 @@ const SuperMap = () => {
   const [switchsX, setSwitchsX] = useState(0);
   const [switchsY, setSwitchsY] = useState(0);
   const [titles, setTitles] = useState<any>({});
-  const [resets, setResets] = useState(false);
   const [fullScreen, setFullScreen] = useState<any>(true);
 
   const [markerList] = useState<any>([
@@ -37,9 +37,10 @@ const SuperMap = () => {
       if (window.viewer?.imageryLayers.length > 1) {
         window.viewer.imageryLayers.removeAll();
       }
-
-      let viewer = new window.Cesium.Viewer('cesiumContainer', {
+      viewer = new window.Cesium.Viewer('cesiumContainer', {
         //初始化
+        navigation: false,
+        infoBox: false,
         terrainProvider: new window.Cesium.CesiumTerrainProvider({
           url: 'http://{s}/realspace/services/3D-dixingyingxiang/rest/realspace/datas/DatasetDEM',
           isSct: true,
@@ -87,10 +88,84 @@ const SuperMap = () => {
       if (viewer.scene) {
         // console.log(viewer.scene);
         setClickEvent(viewer.scene);
+        const scene = viewer.scene;
+        scene.globe.enableLighting = true;
+        scene.sun = new window.Cesium.Sun();
+        scene.sun.glowFactor = 5; // 太阳大小
       }
       //视角变更监控
       cameraChangedListener =
         viewer.camera.changed.addEventListener(cameraChangeListener);
+
+      // //以下是直接打开模型；及配置；
+      // const widget = viewer.cesiumWidget;
+      // try {
+      //   const promise = scene.open(
+      //     // open是直接打开没有动画 Cesium.when()配合使用；
+      //     'http://{s}/realspace/services/3D-NewCBD/rest/realspace',
+      //     undefined,
+      //     {
+      //       subdomains: ['www.supermapol.com'],
+      //     }
+      //   );
+      //   window.Cesium.when(
+      //     promise,
+      //     // // 设置角度位置高度 添加自动定义图标按钮
+      //     // function (layer: any) {
+      //     //   viewer.camera.setView({
+      //     //     destination: window.Cesium.Cartesian3.fromDegrees(
+      //     //       111.51802557938487,
+      //     //       31.010624342873738,
+      //     //       2500
+      //     //     ),
+      //     //     orientation: {
+      //     //       heading: 141.7005863248055,
+      //     //       pitch: window.Cesium.Math.toRadians(-70),
+      //     //       roll: window.Cesium.Math.toRadians(-15),
+      //     //     },
+      //     //   });
+      //     //   viewer.entities.add({
+      //     //     id: 'sx',
+      //     //     show: true,
+      //     //     position: window.Cesium.Cartesian3.fromDegrees(
+      //     //       111.51502557938487,
+      //     //       31.001224342873738,
+      //     //       500
+      //     //     ),
+      //     //     billboard: {
+      //     //       image: '/images/user__easyico.png',
+      //     //       width: 32,
+      //     //       height: 34.8,
+      //     //       pixelOffset: new window.Cesium.Cartesian2(0, -30),
+      //     //     },
+      //     //     name: '三峡大坝',
+      //     //     label: new window.Cesium.LabelGraphics({
+      //     //       text: '三峡大坝',
+      //     //       font: '14px sans-serif bold',
+      //     //       // pixelOffset: new Cesium.Cartesian2(0, 30),
+      //     //       distanceDisplayCondition: {
+      //     //         near: 100,
+      //     //         far: 1500000,
+      //     //       },
+      //     //     }),
+      //     //     description: '',
+      //     //   });
+      //     //   setClickEvent(scene);
+      //     // },
+      //     function (e: any) {
+      //       if (widget._showRenderLoopErrors) {
+      //         const title =
+      //           '加载SCP失败，请检查网络连接状态或者url地址是否正确？';
+      //         widget.showErrorPanel(title, undefined, e);
+      //       }
+      //     }
+      //   );
+      // } catch (e) {
+      //   if (widget._showRenderLoopErrors) {
+      //     const title = '渲染时发生错误，已停止渲染。';
+      //     widget.showErrorPanel(title, undefined, e);
+      //   }
+      // }
     }
 
     if (renderer) {
@@ -106,32 +181,47 @@ const SuperMap = () => {
   }, []);
 
   useEffect(() => {
+    initialization();
+  }, []);
+  const reset = () => {
+    initialization();
+  };
+  const initialization = () => {
+    // 初始化视角
     if (window.viewer?.imageryLayers.length >= 1) {
       // window.viewer.imageryLayers.removeAll();
       window.viewer.camera.flyTo({
-        // 可以初始化视角
         destination: window.Cesium.Cartesian3.fromDegrees(
           954390.6508635766,
-          3330021.765306049,
-          20000000 // 视角深度
+          3330000.765306049,
+          20010000 // 视角深度
         ),
         orientation: {
           // 地球相对窗口方向位置
-          heading: 10,
+          heading: 5,
           pitch: -1.58,
           roll: 2.708944180085382e-13,
         },
         duration: 4,
       });
     }
-  }, [resets]);
+  };
   const setClickEvent = (scene: any) => {
     const handler = new window.Cesium.ScreenSpaceEventHandler(scene.canvas); // 监听点击事件
     if (handler.setInputAction) {
       handler.setInputAction((evt: any) => {
         const pick = scene.pick(evt.position); // 自定义坐标点数据
         var position = scene.pickPosition(evt.position); // 地图坐标
-        // console.log(evt, position, pick);
+        // console.log(
+        //   '地图坐标',
+        //   evt,
+        //   position,
+        //   window.Cesium.Cartesian3.fromDegrees(  // 转换真正地图坐标
+        //     position.x,
+        //     position.y,
+        //     position.z
+        //   )
+        // );
         if (pick?.id) {
           if (infoBoxContainer && infoBoxContainer.current) {
             infoBoxContainer.current.id = pick.id.id;
@@ -155,6 +245,8 @@ const SuperMap = () => {
       const entity = window.viewer.entities.getById(
         infoBoxContainer.current.id
       );
+      console.log(entity);
+      if (!entity) return;
       const Cartesian3 = entity.position._value;
       const position = window.Cesium.SceneTransforms.wgs84ToWindowCoordinates(
         scene,
@@ -166,33 +258,38 @@ const SuperMap = () => {
     }
   };
   const realspace = () => {
-    // window.viewer.scene.open(
-    //   'http://www.supermapol.com/realspace/services/3D-CBD/rest/realspace'
-    // );
+    // setResets(!resets);
     setSwitchsVisibility(false);
-    // var promise = window.viewer.scene.open(
-    //   'http://{s}/realspace/services/3D-NewCBD/rest/realspace',
-    //   undefined,
-    //   {
-    //     subdomains: ['www.supermapol.com'],
-    //   }
-    // );
-    // window.viewer.flyTo(promise);
-
-    var imageLayer = window.viewer.imageryLayers.addImageryProvider(
-      new window.Cesium.SuperMapImageryProvider({
-        url: 'http://{s}/realspace/services/3D-dixingyingxiang/rest/realspace/datas/MosaicResult',
+    const promise: any = window.viewer.scene.open(
+      'http://{s}/realspace/services/3D-NewCBD/rest/realspace',
+      undefined,
+      {
         subdomains: ['www.supermapol.com'],
-        // url: 'https://dev.virtualearth.net',
-        // mapStyle: window.Cesium.BingMapsStyle.AERIAL,
-        // key: window.URL_CONFIG.BING_MAP_KEY,
-      })
+      }
     );
-    window.viewer.flyTo(imageLayer);
+    window.Cesium.when(promise);
+    const scene = viewer.scene;
+    viewer.camera.flyTo({
+      // 角度动画；
+      destination: {
+        x: -2177536.8298188746, // 减小往上移动
+        y: 4381226.303062158, // 加大往左移动
+        z: 4093133.2575022844, // 镜头的高度
+      },
+      // window.Cesium.Cartesian3.fromDegrees( // 转换真正地图坐标
+      //   -2179495.606468646,
+      //   4380208.518509638,
+      //   2500
+      // ),
+      orientation: {
+        heading: 1.7005863248055, // 旋转度数
+        pitch: window.Cesium.Math.toRadians(-20),
+        roll: window.Cesium.Math.toRadians(0),
+      },
+    });
+    setClickEvent(scene);
   };
-  const reset = () => {
-    setResets(!resets);
-  };
+
   const switchs = () => {
     setSwitchsVisibility(false);
   };
